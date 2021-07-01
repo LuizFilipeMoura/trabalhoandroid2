ae62fa014802828478cf3b1609dfb4ab980e0d6e
LuizFilipeMoura <luizfilipeholiday@gmail.com>
2021-06-30 21:44:26 -0300

    .

---
+++ pages/components/Camera.js
@@ -0,0 +1,71 @@
+import React, { PureComponent, useRef } from 'react';
+import {
+  AppRegistry, StyleSheet, Text, TouchableOpacity, View,
+} from 'react-native';
+import { RNCamera } from 'react-native-camera';
+import { useAppContext } from './AppContext';
+
+const Camera = ({ navigation }) => {
+  const camera = useRef();
+  const context = useAppContext();
+
+  const takePicture = async () => {
+    if (camera) {
+      const options = { quality: 0.5, base64: true };
+      const data = await camera.current.takePictureAsync(options);
+      context.setPhotoUrl(data.uri);
+      navigation.navigate('SignUp');
+    }
+  };
+  return (
+    <View style={styles.container}>
+      <RNCamera
+        ref={camera}
+        style={styles.preview}
+        type={RNCamera.Constants.Type.back}
+        captureAudio={false}
+        flashMode={RNCamera.Constants.FlashMode.on}
+        androidCameraPermissionOptions={{
+          title: 'Permission to use camera',
+          message: 'We need your permission to use your camera',
+          buttonPositive: 'Ok',
+          buttonNegative: 'Cancel',
+        }}
+        androidRecordAudioPermissionOptions={{
+          title: 'Permission to use audio recording',
+          message: 'We need your permission to use your audio',
+          buttonPositive: 'Ok',
+          buttonNegative: 'Cancel',
+        }}
+      />
+      <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
+        <TouchableOpacity onPress={takePicture} style={styles.capture}>
+          <Text style={{ fontSize: 14 }}> Capturar </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: 'black',
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    padding: 15,
+    paddingHorizontal: 20,
+    alignSelf: 'center',
+    margin: 20,
+  },
+});
+export default Camera;
